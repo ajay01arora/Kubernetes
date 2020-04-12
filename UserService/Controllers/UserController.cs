3fb29635dd6701e11d833bb0e68f7f4255554ea2
ajay01arora <61735900+ajay01arora@users.noreply.github.com>
2020-04-12 19:15:26 +0530

    UserService

---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace UserService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+       static DataContext.UserDataContext db;
+
+        static UserController()
+        {
+            db = new DataContext.UserDataContext();
+            try
+            {
+                db.Users.Add(new User() { UserId = 1, Name = "Ajay", Age = 23, Email = "ajay@gmail.com" });
+                db.Users.Add(new User() { UserId = 2, Name = "Himanshu", Age = 21, Email = "himanshu@gmail.com" });
+                db.SaveChanges();
+            }catch(Exception ex)
+            {
+
+            }
+        }
+
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+          return db.Users.Find(id);
+        }
+    }
+}
---
+++ UserService/DataContext/UserDataContext.cs
@@ -0,0 +1,19 @@
+﻿    using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.DataContext
+{
+    public class UserDataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(@"data source=AJAY3150060\LUTRON2017; initial catalog=UserMicroService; persist security info=True; user id=sa; password=Lutr0n@1703800293;");
+            optionsBuilder.UseSqlServer(@"Data Source=35.232.25.233,1433;Network Library=DBMSSOCN;Initial Catalog=UserMicroService;User ID=sa;Password=Ajay@110125;");
+        }
+    }
+}
